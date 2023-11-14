69b058e8c7f59cbf6f2ff2c948e747140c514926
hulufei <ihulufei@icloud.com>
2023-11-14 11:58:37 +0800

    Add flash motion

--- plugins/user.fnl
+++ plugins/user.fnl
@@ -7,4 +7,18 @@
  (uu.tx :Olical/conjure {:ft ["clojure" "fennel"]})
  (uu.tx :jaawerth/fennel.vim {:ft ["fennel"]})
  (uu.tx :tpope/vim-repeat {:lazy false})
+ (uu.tx :folke/flash.nvim {:event "VeryLazy"
+                           :opts []
+                           :keys [{1 "S"
+                                   :mode ["n" "x" "o"] 
+                                   2 (fn []  ((. (require "flash") :treesitter)))
+                                   :desc "Flash Treesitter"}
+                                  {1 "R"
+                                   :mode ["x" "o"] 
+                                   2 (fn []  ((. (require "flash") :treesitter_search))) 
+                                   :desc "Treesitter Search"}
+                                  {1 "<c-s>" 
+                                   :mode ["c"] 
+                                   2 (fn []  ((. (require "flash") :toggle))) 
+                                   :desc "Toggle Flash Search"}]})
 ]
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,12 @@
 -- [nfnl] Compiled from plugins/user.fnl by https://github.com/Olical/nfnl, do not edit.
 local uu = require("user.util")
-return {uu.tx("Olical/nfnl", {ft = "fennel"}), uu.tx("Olical/conjure", {ft = {"clojure", "fennel"}}), uu.tx("jaawerth/fennel.vim", {ft = {"fennel"}}), uu.tx("tpope/vim-repeat", {lazy = false})}
+local function _1_()
+  return (require("flash")).treesitter()
+end
+local function _2_()
+  return (require("flash")).treesitter_search()
+end
+local function _3_()
+  return (require("flash")).toggle()
+end
+return {uu.tx("Olical/nfnl", {ft = "fennel"}), uu.tx("Olical/conjure", {ft = {"clojure", "fennel"}}), uu.tx("jaawerth/fennel.vim", {ft = {"fennel"}}), uu.tx("tpope/vim-repeat", {lazy = false}), uu.tx("folke/flash.nvim", {event = "VeryLazy", opts = {}, keys = {{"S", _1_, mode = {"n", "x", "o"}, desc = "Flash Treesitter"}, {"R", _2_, mode = {"x", "o"}, desc = "Treesitter Search"}, {"<c-s>", _3_, mode = {"c"}, desc = "Toggle Flash Search"}}})}
