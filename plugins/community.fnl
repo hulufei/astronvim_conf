2c5a6dfc358b5d43d5f5d9a19fe7a89c48083c85
hulufei <ihulufei@icloud.com>
2023-12-25 18:59:04 +0800

    setup peek.nvim

--- plugins/community.fnl
+++ plugins/community.fnl
@@ -7,6 +7,7 @@
  (uu.tx {:import "astrocommunity.pack.lua"})
  (uu.tx {:import "astrocommunity.pack.typescript-all-in-one"})
  (uu.tx {:import "astrocommunity.pack.markdown"})
+ (uu.tx {:import "astrocommunity.markdown-and-latex.peek-nvim"})
  (uu.tx {:import "astrocommunity.motion.nvim-surround"})
  (uu.tx {:import "astrocommunity.motion.leap-nvim" :enabled false})
  (uu.tx {:import "astrocommunity.note-taking.obsidian-nvim" :enabled false})
@@ -21,4 +22,11 @@
         {:enabled false
          :event [( .. "BufReadPre " vim.env.HOME "/vimwiki/*.md" )]
          :opts {:dir "~/vimwiki"}})
+ (uu.tx :toppair/peek.nvim
+        {:config (fn []
+                   (local peek (require "peek"))
+                   (peek.setup {:app "browser"})
+                   (vim.api.nvim_create_user_command "PeekOpen" peek.open {})
+                   (vim.api.nvim_create_user_command "PeekClose" peek.close {})
+                   )})
 ]
--- plugins/community.lua
+++ plugins/community.lua
@@ -10,4 +10,10 @@ local function _1_(_, opts)
   end
   return nil
 end
-return {uu.tx("AstroNvim/astrocommunity"), uu.tx({import = "astrocommunity.pack.rust"}), uu.tx({import = "astrocommunity.pack.lua"}), uu.tx({import = "astrocommunity.pack.typescript-all-in-one"}), uu.tx({import = "astrocommunity.pack.markdown"}), uu.tx({import = "astrocommunity.motion.nvim-surround"}), uu.tx({import = "astrocommunity.motion.leap-nvim", enabled = false}), uu.tx({import = "astrocommunity.note-taking.obsidian-nvim", enabled = false}), uu.tx("nvim-treesitter/nvim-treesitter", {opts = _1_}), uu.tx("epwalsh/obsidian.nvim", {event = {("BufReadPre " .. vim.env.HOME .. "/vimwiki/*.md")}, opts = {dir = "~/vimwiki"}, enabled = false})}
+local function _3_()
+  local peek = require("peek")
+  peek.setup({app = "browser"})
+  vim.api.nvim_create_user_command("PeekOpen", peek.open, {})
+  return vim.api.nvim_create_user_command("PeekClose", peek.close, {})
+end
+return {uu.tx("AstroNvim/astrocommunity"), uu.tx({import = "astrocommunity.pack.rust"}), uu.tx({import = "astrocommunity.pack.lua"}), uu.tx({import = "astrocommunity.pack.typescript-all-in-one"}), uu.tx({import = "astrocommunity.pack.markdown"}), uu.tx({import = "astrocommunity.markdown-and-latex.peek-nvim"}), uu.tx({import = "astrocommunity.motion.nvim-surround"}), uu.tx({import = "astrocommunity.motion.leap-nvim", enabled = false}), uu.tx({import = "astrocommunity.note-taking.obsidian-nvim", enabled = false}), uu.tx("nvim-treesitter/nvim-treesitter", {opts = _1_}), uu.tx("epwalsh/obsidian.nvim", {event = {("BufReadPre " .. vim.env.HOME .. "/vimwiki/*.md")}, opts = {dir = "~/vimwiki"}, enabled = false}), uu.tx("toppair/peek.nvim", {config = _3_})}
